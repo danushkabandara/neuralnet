b07b1be243696e647ae6e25beea0af78596a8e99
Danushka Bandara <danushka_84@yahoo.com>
2017-11-13 21:04:27 -0500

    update comments

--- convo.py
+++ convo.py
@@ -53,24 +53,17 @@ def load_mnist(path):
 
     return X, y
 
-# here you should enter the path to your MNIST data
+# path to training data
 path = os.path.join(os.path.expanduser('~'), 'Desktop/train.csv')
 
 X, y = load_mnist(path)
 
-#figs, axes = plt.subplots(4, 4, figsize=(6, 6))
-#for i in range(4):
-#    for j in range(4):
-#        axes[i, j].imshow(-X[i + 4 * j].reshape(28, 28), cmap='gray', interpolation='none')
-#        axes[i, j].set_xticks([])
-#        axes[i, j].set_yticks([])
-#        axes[i, j].set_title("Label: {}".format(y[i + 4 * j]))
-#        axes[i, j].axis('off')
+
 layers0 = [
     # layer dealing with the input data
     (InputLayer, {'shape': (None, X.shape[1], X.shape[2], X.shape[3])}),
 
-    # first stage of our convolutional layers
+    # first stage of convolutional layers
     (Conv2DLayer, {'num_filters': 96, 'filter_size': 5}),
     (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
     (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
@@ -78,7 +71,7 @@ layers0 = [
     (Conv2DLayer, {'num_filters': 96, 'filter_size': 3}),
     (MaxPool2DLayer, {'pool_size': 2}),
 
-    # second stage of our convolutional layers
+    # second stage of convolutional layers
     (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
     (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
     (Conv2DLayer, {'num_filters': 128, 'filter_size': 3}),
